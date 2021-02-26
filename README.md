1e69a7fa4ce0d10247d5dd230c6f7008a39c8f55
Mark <77358956+MarkAHoward@users.noreply.github.com>
2021-02-26 14:07:16 -0700

    Creating Files

---
+++ README.md
@@ -0,0 +1,42 @@
+# Batter
+What's your batting average? Find out with this terminal version of the arcade 
+classic. The goal is simple. Just bat the ball at the blricks until there are 
+none left. If you miss the ball the game is over.
+
+## Getting Started
+---
+Make sure you have Python 3.8.0 or newer and asciimatics 1.12.0 or new installed 
+and running on your machine. You can install Asciimatics by opening a terminal 
+and running the following command.
+```
+python3 -m pip install asciimatics
+```
+After you've installed the required libraries, open a terminal and browse to the 
+project's root folder. Start the program by running the following command.
+```
+python3 batter 
+```
+You can also run the program from an IDE like Visual Studio Code. Start your IDE 
+and open the project folder. Select the main module inside the hunter folder and 
+click the "run" icon.
+
+## Project Structure
+---
+The project files and folders are organized as follows:
+```
+root                    (project root folder)
++-- batter              (source code for game)
+  +-- game              (specific game classes)
+  +-- __init__.py       (python package file)
+  +-- __main__.py       (entry point for program)
++-- README.md           (general info)
+```
+
+## Required Technologies
+---
+* Python 3.8.0
+* Asciimatics 1.12.0
+
+## Authors
+---
+* # TODO: Add your names and emails here
---
+++ batter/__main__.py
@@ -0,0 +1,64 @@
+import random
+from game import constants
+from game.director import Director
+from game.actor import Actor
+from game.point import Point
+from game.control_actors_action import ControlActorsAction
+from game.draw_actors_action import DrawActorsAction
+from game.handle_collisions_action import HandleCollisionsAction
+from game.move_actors_action import MoveActorsAction
+from game.input_service import InputService
+from game.output_service import OutputService
+from asciimatics.screen import Screen 
+
+def main(screen):
+
+    # create the cast {key: tag, value: list}
+    cast = {}
+
+    x = int(constants.MAX_X / 2)
+    y = int(constants.MAX_Y - 1)
+    position = Point(x, y)
+    paddle = Actor()
+    paddle.set_text("===========")
+    paddle.set_position(position)
+    cast["paddle"] = [paddle]
+
+    cast["brick"] = []
+    for x in range(5, 75):
+        for y in range(2, 6):
+            position = Point(x, y)
+            brick = Actor()
+            brick.set_text("*")
+            brick.set_position(position)
+            cast["brick"].append(brick)
+
+    x = int(constants.MAX_X / 2)
+    y = int(constants.MAX_Y / 2)
+    position = Point(x, y)
+    velocity = Point(1, -1)
+    ball = Actor()
+    ball.set_text("@")
+    ball.set_position(position)
+    ball.set_velocity(velocity)
+    cast["ball"] = [ball]
+    
+    # create the script {key: tag, value: list}
+    script = {}
+
+    input_service = InputService(screen)
+    output_service = OutputService(screen)
+    control_actors_action = ControlActorsAction(input_service)
+    move_actors_action = MoveActorsAction()
+    handle_collisions_acition = HandleCollisionsAction()
+    draw_actors_action = DrawActorsAction(output_service)
+    
+    script["input"] = [control_actors_action]
+    script["update"] = [move_actors_action, handle_collisions_acition]
+    script["output"] = [draw_actors_action]
+
+    # start the game
+    director = Director(cast, script)
+    director.start_game()
+
+Screen.wrapper(main)
