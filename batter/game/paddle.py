1217f3bcd1109d11fa2a29c1b061dee4a13a48eb
Conner Haar <77696186+Connerhaar@users.noreply.github.com>
2021-02-26 19:42:47 -0700

    paddle 1.0

--- batter/game/paddle.py
+++ batter/game/paddle.py
@@ -1,12 +1,22 @@
 from game.actor import Actor
+from game import constants
+from game.point import Point
 
-class Paddle:
-    def __init__(self)
+class Paddle(Actor):
+    def __init__(self):
         super().__init__()
         x = int(constants.MAX_X / 2)
         y = int(constants.MAX_Y - 1)
         position = Point(x, y)
         self.set_text("===========")
         self.set_position(position)
+
+    def move_right(self):
+        pass
+    def move_left(self):
+        pass
+
+    
+
 
 
